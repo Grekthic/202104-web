b5e83414cb20e9d3860c4df4b74fde92c07504d9
otro <r2d2klapa@gmail.com>
2021-10-21 02:38:54 +0000

    Ejemplos con SASS

--- DemoBootstrap/DemoBootstrap.WebMvc/Controllers/HomeController.cs
+++ DemoBootstrap/DemoBootstrap.WebMvc/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace DemoBootstrap.WebMvc.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult DemoSASS()
+        {
+            return View();
+        }
     }
 }
